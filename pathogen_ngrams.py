cacfb5b7be3e0dec4717366a8dec8f5944e9f764
Leigh Yeh <lpyeh@umich.edu>
2018-10-19 11:40:55 -0700

    pathogen_ngrams now imports getngrams

--- pathogen_ngrams.py
+++ pathogen_ngrams.py
@@ -1,11 +1,15 @@
+#!/usr/bin/python
 import requests
 from collections import defaultdict
 import re
 import json
-from ast import literal_eval
-import ahocorasick
+# from ast import literal_eval
+# import ahocorasick
 import pickle
 import os
+from getngrams import run
+import shutil
+
 
 # Returns list of words to use as target words 
 def dictionary_text(url):
@@ -32,14 +36,18 @@ def dictionary_text(url):
     return MFD
 
 
+def get_words(path):
+    word_list = open(path).read().splitlines()
+    # print(word_list)
+    return word_list
+'''
 # Returns list of words to use as base words (AKA pathogen prevalence words)
 def pathogen_text(path):
     pathogen_list = []
     return pathogen_list
 
 
-def complete_dictionary(text_list):
-
+def complete_dictionary(text_list)
     with open('/Users/leigh/Desktop/words_dictionary.json') as words:
         words_dictionary = json.load(words)
 
@@ -51,45 +59,29 @@ def complete_dictionary(text_list):
     for item in text_list:
         print(item)
         print(list(A.keys(item)))
+'''
 
-
-# getNgrams function, modified from https://github.com/encopy/google-ngrams
-def getNgrams(query):
-    eng_us_2012_corpus = 17
-    params = dict(content = query, corpus=eng_us_2012_corpus, year_start=1800, year_end=2000)
-
-    req = requests.get('http://books.google.com/ngrams/graph', params=params)
-    res = re.findall('var data = (.*?);\\n', req.text)
-
-    if res:
-        data = {qry['ngram']: qry['timeseries'] for qry in literal_eval(res[0])}
-        df = DataFrame(data)
-        df.insert(0, 'year', list(range(year_start, year_end + 1)))
-    else:
-        df = DataFrame()
-
-    return params['content'], df
-
-
-# runQuery function, modified from https://github.com/encopy/google-ngram
-def runQuery(string):
-    filename = '%s-%s-%d-%d.csv' % (string, "eng_us_2012_corpus", 1800, 2000)
-    urlquery, df = getNgrams(string)
-    
-    for col in df.columns:
-        if '&gt;' in col:
-            df[col.replace('&gt;', '>')] = df.pop(col)
-        df.to_csv(filename, index=False)
-        print('Data saved to %s' % filename)
+def merge_csvs():
+    allFiles = glob.glob(os.getcwd() + "/*.csv")
+    with open('Dictionary.csv', 'wv') as outfile:
+        for i, fname in enumerate(allFiles):
+            with open(fname, 'rb') as infile:
+                if i != 0:
+                    infile.readline()
+                shutil.copyfileobj(infile, outfile)
 
 
 if __name__ == '__main__':
-    MFD_url = \
-        "https://www.moralfoundations.org/sites/default/files/files/downloads/moral%20foundations%20dictionary.dic"
-    pathogen_path = "Put path here"
+    # pathogen_path = "Put path here"
+    base_words_path = "/Users/leighyeh/Desktop/pathogen_prevalence_code/MFD.txt"
+    base_words = get_words(base_words_path)
+    
+    if len(base_words) > 10:
+        j = 0
+        while j < len(base_words):
+            run(base_words[j:j + 10])
+            j += 10
 
-    target_words = dictionary_text(MFD_url)
-    # base_words = pathogen_text(pathogen_path)
-    complete_dictionary(target_words)
-    query = ' '.join([word for word in target_words])
-    # print(target_words)
+    else:
+        run(base_words)
+    # merge_csvs(output_path)
