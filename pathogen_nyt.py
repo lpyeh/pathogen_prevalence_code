0446b6b9be514bd4da9d5e026fbd97201432c746
lpyeh <lpyeh@umich.edu>
2019-03-26 12:46:58 -0700

    updated code to add proper # of years to df

--- pathogen_nyt.py
+++ pathogen_nyt.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python
 
-import pandas
+import pandas as pd
 import pickle
 import os
 import zipfile
@@ -8,13 +8,13 @@ import re
 import pathlib
 from collections import defaultdict
 from collections import Counter
+import csv
 
 '''
 Author: Leigh Yeh
 Use the NYT corpus to find word frequency given words from the MFD.
 '''
-
-
+'''
 # Only need to use this if zip files aren't extracted
 def extract_zip():
     directory = 'txtOnly'
@@ -26,29 +26,57 @@ def extract_zip():
         zip_ref.extractall(file_name)
         zip_ref.close()
         os.remove(file_name)
+'''
+pattern = re.compile('[\W_]+', re.UNICODE)
 
 
-def get_nyt(dictionary):
-    frequency_dict = defaultdict(int)
-    directory = 'NYT'
-    os.chdir(directory)
+def get_nyt(dictionary, path, year_in):
+    month_df = pd.DataFrame()
+    for month in os.listdir(path):
+        if not month.startswith('.'):
+            print(month)
+            month_path = os.path.join(path, month)
+            frequency_dict = defaultdict(int)
+            for file in os.listdir(month_path):
+                if not file.startswith('.'):
+                    file_path = os.path.join(month_path, file)
+                    with open(file_path, encoding='utf-8-sig') as in_file:
+                        in_file = pattern.sub(' ', in_file.read())
+                        in_file = in_file.lower().strip()
+                        # print(in_file)
+                        wordcount = Counter(in_file.split())
+                        for word in dictionary:
+                            if word in wordcount:
+                                frequency_dict[word] += wordcount[word]
+                            else:
+                                frequency_dict[word] += 0
+            dataframe = pd.DataFrame(frequency_dict, index=[month])
+            month_df = month_df.append(dataframe, sort=False)
+    return month_df
 
-    for year in os.listdir(os.getcwd()):
-        for root, dirs, files in os.walk(year):
-            for root2, dirs2, files2 in os.walk(root):
-                root_path = os.path.abspath(root2)
-                for file in files2:
-                    file_path = os.path.join(root_path, file)
-                    with open(file_path, 'rb') as f:
-                        contents = f.read()
-                        contents.decode()
-                        print(contents)
 
 if __name__ == '__main__':
-    path = 'MFD.txt'
+    path = 'long_mfd.txt'
     mfd = open(path).read().lower().splitlines()
-    d = ['this', 'is', 'a', 'test']
-    get_nyt(d)
+    mfd_df = pd.DataFrame()
+    mfd = {k: 0 for k in mfd}
+    year_list = []
+    year_col = []
+    os.chdir('NYT')
 
+    for year in os.listdir(os.getcwd()):
+        if not year.startswith('.') and not year.endswith('.pkl'):
+            year_list.append(year)
+            in_path = os.path.join(os.getcwd(), year)
+            if in_path.endswith('.csv'):
+                continue;
+            df1 = get_nyt(mfd, in_path, year)
+            mfd_df = mfd_df.append(df1)
+        with open(year + '.pkl', 'wb') as out_file:
+            pickle.dump(df1, out_file)
+    year_col = [[x] * 12 for x in year_list]
+    year_col = [a for b in year_col for a in b]
 
+    mfd_df['Year'] = year_col
 
+    mfd_df.to_csv("nyt_mfd_long.csv")
